9a43c9c08f829378c972522a9eaf3f6e999c71e9
savage <2443005004@qq.com>
2023-09-03 03:22:07 +0800

    ci: change script

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -5,11 +5,12 @@ import apidocConfig from '../apidocConfig.json'
 import pkg from '../../package.json'
 
 const name = pkg.name
+const path = name.replace(/([a-z]+-)/, 'npm-')
 
 export default defineConfig({
-	base: `/npm-data-types/`,
+	base: `/${path}/`,
 	title: `${name}`,
-	head: [[`link`, { rel: `icon`, href: `/${name}/savage.ico` }]],
+	head: [[`link`, { rel: `icon`, href: `/${path}/savage.ico` }]],
 	themeConfig: {
 		logo: {
 			src: '/savage.png',
